378467e8404276488cd176656d27b9c39ee8068c
rahilhasnani95 <rahilhasnani95@gmail.com>
2019-06-13 01:18:41 -0500

    added day1 assignment1

---
+++ Adv_Day1/src/Assignment2/EmployeeGrade.java
@@ -0,0 +1,51 @@
+package Assignment2;
+
+
+
+public class EmployeeGrade {
+
+	public static void main(String[] args) {
+	
+		int []employeeNo = {1,2,3,4,5};
+		float []customer1Feedback = {4.1f, 4.2f, 4.3f, 4.4f, 4.5f};
+		float []customer2Feedback = {4,5,4,4.8f,4};
+		float []customer3Feedback = {5,5,4,3,5};
+		float[]averageFeedback = new float[employeeNo.length];
+		char []grade = new char[employeeNo.length];
+		
+		for(int i = 0; i < employeeNo.length; i++)
+		{
+			averageFeedback[i] = (customer1Feedback[i] + customer2Feedback[2] + customer3Feedback[i])/3;
+			//System.out.println(averageFeedback[i]);
+		}
+		for(int i = 0; i < averageFeedback.length; i++)
+		{
+			if(averageFeedback[i] > 4.5)
+			{
+				grade[i] = 'A';
+			}
+			else if(averageFeedback[i] <= 4.5 && averageFeedback[i] > 4.0f)
+			{
+				grade[i] = 'B';
+			} 
+			else if(averageFeedback[i] <=4.0 && averageFeedback[i] > 3.5f)
+			{
+				grade[i] = 'C';
+			}
+			else
+			{
+				grade[i] = 'D';
+			}
+		}
+		for(int i = 0; i < employeeNo.length; i++)
+		{
+			System.out.print("\t" +averageFeedback[i]);
+			System.out.println("\t" + grade[i]);
+		}
+
+
+
+
+		
+	}
+}
